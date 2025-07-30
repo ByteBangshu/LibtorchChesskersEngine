d92c9deb9d85a3c7e6c20fe43fc4bf2dc76e7706
bytebangshu <mail.debangshupramanik@gmail.com>
2025-07-30 13:00:46 -0700

    Sync after first real commit.

---
+++ build/CMakeFiles/torch_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/torch_test.dir/main.cpp.o"
+  "CMakeFiles/torch_test.dir/main.cpp.o.d"
+  "torch_test"
+  "torch_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/torch_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
